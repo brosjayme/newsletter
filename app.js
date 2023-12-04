3202c37f59a3097047e0f615ee3691f2326ddb1a
unknown <brosjayme@gmail.com>
2023-12-04 08:24:53 +0100

    added the index page to the app

--- app.js
+++ app.js
@@ -4,10 +4,39 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  var firstName = req.body.fname;
+  var lastName = req.body.lname;
+  var email = req.body.email;
+  //   console.log(firstName, lastName, email);
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
 });
 
 app.listen(3000, function (req, res) {
   console.log("Server is running on port 3000");
 });
+
+// API Key
+// 6b48785c45f19ee13cbe06b3d411bbd2-us21
+
+// list Id
